9893ec9b9167d6745df0e81164494f0d1fbf5c17
Kimmo Hintikka <hintikkakimmo@gmail.com>
2018-05-09 21:59:04 +0100

    progress so far video 70

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import './style.scss';
+
+new Vue({
+    el: '#app',
+    components: {
+        "movie-list": {
+            template: `<div id="movie-list">
+<div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+</div>`,
+            data: function () {
+                return {
+                    movies: [
+                        { title: 'Pulp Fiction' },
+                        { title: 'Home Alone' },
+                        { title: 'Austin Powers' }
+                        ]
+                };
+            }
+            },
+        "movie-filter": {
+            template:`<div id="movie-filter">
+<h2>Filter results</h2>
+<check-fitler></check-fitler>
+</div>`,
+            components: {
+                'check-filter': {
+                    template: `<div>Filter</div>`
+                }
+            }
+        }
+    }
+});
