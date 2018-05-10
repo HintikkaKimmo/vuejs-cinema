0c9573b29c98cf4b0dcb3249a4f155234ae1fd4a
Kimmo Hintikka <hintikkakimmo@gmail.com>
2018-05-10 19:43:01 +0100

    video 73 so far code has algiment bug

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres'
 
 new Vue({
     el: '#app',
     components: {
         "movie-list": {
             template: `<div id="movie-list">
-<div v-for="movie in movies" class="movie">{{ movie.title }}</div>
-</div>`,
-            data: function () {
+                        <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                       </div>`,
+            data() {
                 return {
                     movies: [
                         { title: 'Pulp Fiction' },
@@ -19,13 +20,29 @@ new Vue({
             }
             },
         "movie-filter": {
+            data() {
+                return {
+                    genres
+                }
+            },
             template:`<div id="movie-filter">
-<h2>Filter results</h2>
-<check-fitler></check-fitler>
-</div>`,
+                        <h2>Filter results</h2>
+                        <div class="filter-group">
+                        <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>                       
+                        </div>
+                      </div>`,
             components: {
-                'check-filter': {
-                    template: `<div>Filter</div>`
+                "check-filter": {
+                    data() {
+                        return {
+                            checked: false
+                        }
+                    },
+                    props: [ 'title' ],
+                    template: `<div v-bind:class=""{ 'check-filter': true, active: checked }" v-on:click="checked = !checked">
+                                <span class="checkbox"></span>
+                                <span class="check-filter-title">{{ title }}</span>
+                               </div>`
                 }
             }
         }
